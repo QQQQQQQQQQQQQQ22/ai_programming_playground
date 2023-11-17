2c33d93d651e4b0ff0655b353da23a7b74f0f955
dmytro-Mel <dmitrom125@gmail.com>
2023-11-17 20:47:04 +0200

    renamed files and edit

---
+++ ai_13/dmytro_melnykov/practice_Epic_2/Epic_2_algo_lab_1_Melnykov_Dmytro.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+    long long a[5];
+    for (int i = 0; i < 5; i++)
+    {
+
+        std::cin >> a[i];
+        if(a[i] <= 0)
+        {
+            std::cout<< "ERROR";
+            return 0;
+            
+        }
+        if(i >= 4 && a[i - 1] >= a[i])
+        {
+            std::cout << "WIN";
+            return 0;    
+        }
+        else if(i >= 1 && a[i] > a[i - 1])
+        {
+            std::cout << "LOSS";
+            return 0;
+        }  
+    }
+
+}
+
+
+
+
